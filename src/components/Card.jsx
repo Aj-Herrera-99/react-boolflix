4ff54da64b3f7750e4032fe5c596a3c3a13ab422
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-15 02:32:05 +0100

    preso FlagsApi per visualizzare le bandiere dei paesi delle lingue (API provvisorio)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,36 @@ import React from "react";
 import { api_img_url } from "../globals/globals";
 
 function Card({ type, media }) {
+    let {
+        title,
+        original_title,
+        name,
+        original_name,
+        original_language,
+        poster_path,
+        vote_average,
+    } = media;
+    if (original_language == "en") original_language = "gb";
+
     return (
         <div className="bg-blue-600 ">
             <img
-                src={`${api_img_url}/w185${media.poster_path}`}
-                alt={type === "movie" ? media.title : media.name}
+                src={`${api_img_url}/w185${poster_path}`}
+                alt={type === "movie" ? title : name}
             />
-            <p>Title: {type === "movie" ? media.title : media.name}</p>
-            <p>Original Title: {type === "movie" ? media.original_title : media.original_name}</p>
-            <p>Original Language: {media.original_language}</p>
-            <p>Vote: {media.vote_average}</p>
+            <p>Title: {type === "movie" ? title : name}</p>
+            <p>
+                Original Title:{" "}
+                {type === "movie" ? original_title : original_name}
+            </p>
+            <p>
+                Original Language:{" "}
+                <img
+                    width={32}
+                    src={`https://flagsapi.com/${original_language.toUpperCase()}/shiny/32.png`}
+                ></img>
+            </p>
+            <p>Vote: {vote_average}</p>
         </div>
     );
 }
