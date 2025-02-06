4a81bc034ee75c64833e804c293657b9489f6b32
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-06 04:22:12 +0100

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React, { lazy, Suspense } from "react";
-import "./App.css";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-import DefaultLayout from "./pages/DefaultLayout";
-const LazyHomepage = lazy(() => import("./pages/Homepage"));
-import SearchMedia from "./pages/SearchMedia";
-const LazyDetailPage = lazy(() => import("./pages/DetailPage"));
-import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Loader from "./components/Loader";
+import DefaultLayout from "./pages/DefaultLayout";
+import SearchMedia from "./pages/SearchMedia";
+import NotFound from "./pages/NotFound";
+const LazyHomepage = lazy(() => import("./pages/Homepage"));
+const LazyDetailPage = lazy(() => import("./pages/DetailPage"));
 
 const queryClient = new QueryClient();
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
                     <Route Component={DefaultLayout}>
                         <Route
@@ -38,7 +37,7 @@ function App() {
                         <Route path="*" Component={NotFound}></Route>
                     </Route>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
             <ReactQueryDevtools />
         </QueryClientProvider>
     );
